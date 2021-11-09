9fe9d97b3a7677feaf8c8fc9f330e721585c4414
Adnan Arabadzhi <adnanarabadji@gmail.com>
2021-11-09 23:57:50 +0200

    fixed url

--- environment.js
+++ environment.js
@@ -1,2 +1,5 @@
-export const GRAPHQL_API_URL = 'http://vetonstudio.com/graphql';
-export const API_URL = 'http://vetonstudio.com';
+// export const GRAPHQL_API_URL = 'http://vetonstudio.com/graphql';
+// export const API_URL = 'http://vetonstudio.com';
+
+export const GRAPHQL_API_URL = 'http://localhost:1337/graphql';
+export const API_URL = 'http://localhost:1337';
