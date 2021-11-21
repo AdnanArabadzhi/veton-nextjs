63445d03ef3bbe1922ca62a86c6e67fee28ac19f
Adnan Arabadzhi <adnanarabadji@gmail.com>
2021-11-21 19:26:25 +0200

    testing domains

--- environment.js
+++ environment.js
@@ -4,5 +4,5 @@
 // export const GRAPHQL_API_URL = 'http://localhost:1337/graphql';
 // export const API_URL = 'http://localhost:1337';
 
-export const GRAPHQL_API_URL = 'http://vetonstudio.com/graphql';
-export const API_URL = 'http://vetonstudio.com';
+export const GRAPHQL_API_URL = 'http://admin.vetonstudio.com/graphql';
+export const API_URL = 'http://admin.vetonstudio.com';
