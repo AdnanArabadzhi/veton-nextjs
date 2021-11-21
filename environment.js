7fa2b6db4c92f54f68ee2e2d5119ab1455e268cd
Adnan Arabadzhi <adnanarabadji@gmail.com>
2021-11-21 19:22:21 +0200

    testing domains

--- environment.js
+++ environment.js
@@ -1,5 +1,8 @@
 // export const GRAPHQL_API_URL = 'http://vetonstudio.com/graphql';
 // export const API_URL = 'http://vetonstudio.com';
 
-export const GRAPHQL_API_URL = 'http://localhost:1337/graphql';
-export const API_URL = 'http://localhost:1337';
+// export const GRAPHQL_API_URL = 'http://localhost:1337/graphql';
+// export const API_URL = 'http://localhost:1337';
+
+export const GRAPHQL_API_URL = 'http://vetonstudio.com/graphql';
+export const API_URL = 'http://vetonstudio.com';
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'vetonstudio.com'],
+    domains: ['localhost', 'vetonstudio.com', 'admin.vetonstudio.com'],
   },
 };
